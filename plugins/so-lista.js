7df12bd3439db1461558e3b47731fff7ba3393b2
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-08-29 00:41:05 +0200

    Add files via upload

--- plugins/so-lista.js
+++ plugins/so-lista.js
@@ -13,12 +13,6 @@ let vn = 'https://qu.ax/xynz.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })}  
 
-if (/^Chupame|Pingo|Qliao$/i.test(m.text) && chat.audios) {  
-if (!db.data.chats[m.chat].audios && m.isGroup) throw 0    
-let vn = 'https://qu.ax/SCpi.mp3'
-this.sendPresenceUpdate('recording', m.chat)   
-this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })}  
-
 if (/^ara ara$/i.test(m.text) && chat.audios) {  
 let vn = 'https://qu.ax/PPgt.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
@@ -674,5 +668,10 @@ let vn = 'https://qu.ax/pXts.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })}
    
+if (chat.audios && m.text.match(/(@56941827124|@56921994449)/gi)) {    
+let vn = './media/geri.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })}
+    
 return !0 }
 export default handler
